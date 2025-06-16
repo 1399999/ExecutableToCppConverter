22adcb60f3a7ef391c3f8edb755707eef0a1f627
1399999 <mzhebrunov@gmail.com>
2025-06-16 15:35:37 -0400

    Adds a very simple implementation of the program.

---
+++ ExeToCpp/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("================ Executable to C++ Converter ================\n");
+
+        while (true) 
+        {
+            Console.Write("> ");
+            string input = Console.ReadLine();
+
+            if (File.Exists(input))
+            {
+                Console.WriteLine();
+
+                byte[] executableContents = File.ReadAllBytes(input);
+
+                for (int i = 0; i < executableContents.Length; i++)
+                {
+                    Console.Write($"{executableContents[i]} ");
+
+                    if (i % 16 == 15)
+                    {
+                        Console.WriteLine();
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
