e8a94188d09753ac575d2e27800cad6df401f6bf
1399999 <mzhebrunov@gmail.com>
2025-06-22 16:39:46 -0400

    Implements the main functionality for the Value class.

--- ExeToCpp/NeuralNetworkEngine/Value.cs
+++ ExeToCpp/NeuralNetworkEngine/Value.cs
@@ -7,15 +7,17 @@ public class Value
     public double Data { get; set; }
     public double Grad { get; set; }
     public string Op { get; set; }
-    public (Value, Value)? Prev { get; set; }
-    public static Backward? BackwardFunction { get; set; }
+    public List<Value>? Prev { get; set; }
+    public Backward BackwardFunction { get; set; }
 
-    public Value(double data, (Value, Value)? children = null, string op = "")
+    public Value(double data, List<Value>? children = null, string op = "")
     {
         Data = data;
         Grad = 0;
         Prev = children;
         Op = op; // Vestigial variable.
+
+        BackwardFunction = () => {};
     }
 
     public static Value operator +(Value self, Value other) => PlusCommon(self, other);
@@ -24,9 +26,9 @@ public class Value
 
     private static Value PlusCommon(Value self, Value other)
     {
-        var output = new Value(self.Data + other.Data, (self, other), "+");
+        var output = new Value(self.Data + other.Data, new List<Value>(2) { self, other }, "+");
 
-        BackwardFunction = () => 
+        output.BackwardFunction = () => 
         {
             self.Grad += output.Grad;
             other.Grad += output.Grad;
@@ -41,9 +43,9 @@ public class Value
 
     private static Value MulCommon(Value self, Value other)
     {
-        var output = new Value(self.Data * other.Data, (self, other), "*");
+        var output = new Value(self.Data * other.Data, new List<Value>(2) { self, other }, "*");
 
-        BackwardFunction = () =>
+        output.BackwardFunction = () =>
         {
             self.Grad += other.Data * output.Grad;
             other.Grad += self.Data * output.Grad;
@@ -54,13 +56,64 @@ public class Value
 
     public static Value operator ^(Value self, double other)
     {
-        var output = new Value(Math.Pow(self.Data, other), (self, new Value(int.MinValue)), "*");
+        var output = new Value(Math.Pow(self.Data, other), new List<Value>(1) { self }, "*");
 
-        BackwardFunction = () =>
+        output.BackwardFunction = () =>
         {
             self.Grad += (other * Math.Pow(self.Data, other - 1)) * output.Grad;
         };
 
         return output;
+    }
+
+    public Value Relu()
+    {
+        Value output = new Value(Data < 0 ? 0 : Data, new List<Value>(1) { this }, "ReLU");
+
+        BackwardFunction = () =>
+        {
+            Grad += output.Data > 0 ? output.Data * output.Grad : 0;
+        };
+
+        return output;
+    }
+
+    public void BackwardPropagate()
+    {
+        List<Value> grandList = new List<Value>();
+        List<Value> visited = new List<Value>();
+
+        // Builds a topological order all of the children in the graph.
+        BuildTopologicalList(grandList, visited, this);
+
+        // Go one variable at a time and apply the chain rule to get its gradient.
+        Grad = 1;
+
+        grandList.Reverse();
+
+        foreach (var grand in grandList) 
+        {
+            grand.BackwardFunction();
+        }
+    }
+
+    private static void BuildTopologicalList(List<Value> grandList, List<Value> visited, Value value)
+    {
+        if (value.Prev == null)
+        {
+            return;
+        }
+
+        if (!visited.Contains(value)) 
+        {
+            visited.Add(value);
+
+            foreach (var child in value.Prev)
+            {
+                BuildTopologicalList(grandList, visited, child);
+            }
+
+            grandList.Add(value);
+        }
     }
 }
